aabd0873d9704b61de60194a135cd3f1323d99c9
moin1990 <kemoin@gmail.com>
2026-05-22 01:10:22 +0600

    update client url

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://localhost:3000',
-    process.env.CLIENT_URL || 'https://drass94-client.vercel.app/'
+    process.env.CLIENT_URL || 'https://assignment-nine-client.vercel.app/'
   ],
   credentials: true
 }));
